95a805875fe5064fd19c0a8cce1197b2cfdfe19a
NhatTuan249 <nhattuanng99@gmail.com>
2024-11-30 11:23:09 +0700

    first upload

---
+++ routes/indexRouter.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/indexController');
+
+//routes
+/*
+router.get('/createTables', (req, res) => {
+    let models = require('../models');
+    models.sequelize.sync().then(() => {
+        res.send('tables created!');
+    });
+});
+*/
+
+router.get('/', controller.showHomepage);
+
+router.get('/:page', controller.showPage);
+
+/* router.get('/:page', (req, res) => {
+    res.render(req.params.page)
+}); */
+
+module.exports = router;
